81924b6e16c4495085beb3705fa7a5426ac81b47
robertvlad <vladrob96@gmail.com>
2023-05-05 11:48:51 +0300

    Snack 1

---
+++ src/org/lessons/java/JavaSnack1.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class JavaSnack1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero");
+		
+		int num = sc.nextInt();		
+		
+		System.out.println(num % 2 == 0 ? num : (num + 1));
+		
+		sc.close();
+		
+	}
+
+}
