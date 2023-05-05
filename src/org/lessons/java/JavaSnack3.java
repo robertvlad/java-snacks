1a2dc7057eac7749e529b1a75fc1160638c320c7
robertvlad <vladrob96@gmail.com>
2023-05-05 12:23:57 +0300

    Snack 3

--- src/org/lessons/java/JavaSnack3.java
+++ src/org/lessons/java/JavaSnack3.java
@@ -1,5 +1,30 @@
 package org.lessons.java;
 
 public class JavaSnack3 {
-
+	
+	public static void main(String[] args) {
+		
+		int[] numeri = {
+				1,
+				22,
+				33,
+				45,
+				64
+		};
+		
+		int somma = 0; 
+		
+		for ( int i = 0; i < numeri.length; i++) {
+			
+			int numero = numeri[i];
+			
+			if (numero % 2 != 0) {
+				
+				somma += numero;
+			}
+		}
+		
+		System.out.println(somma);
+	
+	}
 }
--- src/org/lessons/java/JavaSnack4.java
+++ src/org/lessons/java/JavaSnack4.java
@@ -1,5 +1,11 @@
 package org.lessons.java;
 
 public class JavaSnack4 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
 
 }
