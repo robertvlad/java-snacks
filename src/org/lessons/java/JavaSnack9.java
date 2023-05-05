88b106641939f4385e821ecf9ea7c8a36d05defb
robertvlad <vladrob96@gmail.com>
2023-05-05 17:46:21 +0300

    Snack 9

--- src/org/lessons/java/JavaSnack9.java
+++ src/org/lessons/java/JavaSnack9.java
@@ -1,5 +1,27 @@
 package org.lessons.java;
 
-public class JavaSnack9 {
+import java.util.Scanner;
 
+public class JavaSnack9 {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Defini l'altezza e la base");
+		
+		int h = sc.nextInt(),
+			b = sc.nextInt();
+		
+		System.out.println("La base scelta è: " + b + " e l'altezza è: " + h);
+		
+		int a = b * h,
+			p = (b + h) * 2;
+		
+		System.out.println("L'area del rettangolo è: " + a);
+		System.out.println("Il perimetro del rettangolo è: " + p);
+		
+		sc.close();
+	}
 }
