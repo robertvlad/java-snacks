2596fc87d0648c037d35948cb8001f62c6b100ac
robertvlad <vladrob96@gmail.com>
2023-05-05 16:10:16 +0300

    Snack 6

--- src/org/lessons/java/JavaSnack6.java
+++ src/org/lessons/java/JavaSnack6.java
@@ -1,10 +1,41 @@
 package org.lessons.java;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class JavaSnack6 {
 	
 	public static void main(String[] args) {
 		
-				
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Scelga il limite della somma con un numero intero positivo");
+		
+		int userNum  = sc.nextInt();
+		
+		System.out.println("Lei ha scelto come limite il numero: " + userNum);
+		
+		Random rand = new Random();
+			
+		int somma = 0,
+				randNumOut = 0;
+		
+		while (somma <= userNum) {
+			
+			int randNum = rand.nextInt(101);
+			
+			randNumOut = randNum;
+			
+			System.out.println(randNum);
+			
+			somma += randNum;
+		}
+		
+		somma -= randNumOut;
+		
+		System.out.println("La somma più vicina al limite è: " + somma);
+		
+		sc.close();
 	}
 
 }
