7e7feb2707a7153fc10a79720a769b3216970bd5
robertvlad <vladrob96@gmail.com>
2023-05-08 15:00:10 +0300

    Snack 12

--- src/org/lessons/java/JavaSnack12.java
+++ src/org/lessons/java/JavaSnack12.java
@@ -1,5 +1,29 @@
 package org.lessons.java;
 
-public class JavaSnack12 {
+import java.util.Arrays;
 
+public class JavaSnack12 {
+	
+	public static void main(String[] args) {
+		
+		Integer[] arr1 = { 1, 2, 3, 4, 5 };
+		Integer[] arr2 = { 34, 35, 36, 37, 38 };
+		
+		int arr1lng = arr1.length;
+		int arr2lng = arr2.length;
+		
+		Integer[] arr3 = new Integer[arr1lng + arr2lng];
+		
+		for ( int i = 0; i < arr1lng; i++) {
+			
+			arr3[i] = arr1[i];
+		}
+		
+		for ( int i = 0; i < arr2lng; i++) {
+			
+			arr3[i + 5] = arr2[i];
+		}
+		
+		System.out.println(Arrays.asList(arr3));		
+	}
 }
