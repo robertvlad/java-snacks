8a37cff67859891b7784834d4b0297bbe616a19f
robertvlad <vladrob96@gmail.com>
2023-05-08 15:03:54 +0300

    Snack 13

--- src/org/lessons/java/JavaSnack13.java
+++ src/org/lessons/java/JavaSnack13.java
@@ -1,5 +1,21 @@
 package org.lessons.java;
 
-public class JavaSnack13 {
+import java.util.Arrays;
 
+public class JavaSnack13 {
+	
+	public static void main(String[] args) {
+		
+		Integer[] arr1 = { 1, 2, 3, 4, 5 };
+		Integer[] arr2 = { 34, 35, 36, 37, 38 };
+		
+		int n1 = arr1[0];
+		int n2 = arr2[0];
+		
+		arr1[0] = n2;
+		arr2[0] = n1;
+		
+		System.out.println(Arrays.asList(arr1));
+		System.out.println(Arrays.asList(arr2));
+	}
 }
